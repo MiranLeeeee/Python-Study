8241f5f665d6a6c3a3f2aeea052161a4a171b6e3
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-06-04 11:01:42 +0900

    upload basic_string.py

---
+++ string/basic_string.py
@@ -0,0 +1,14 @@
+'''
+문자열은 초기화할때 큰 따옴표, 작은 따옴표 둘다 사용가능
+전체문자열이 큰 따옴표(작은 따옴표) 로 구성한 경우 내부에 작은 따옴표(큰 따옴표) 가능
+\사용 시 원하는 만큼 따옴표 사용가능
+'''
+
+str = 'Hello World!'
+print(str)
+
+str = "Hello World!"
+print(str)
+
+str = "\"Hello\" World!"
+print(str)
