7d4a70cacdeb97d4a3a24c2db04f4c7b1d4b20de
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-06-22 21:36:20 +0900

    Create comparison_operator.py

---
+++ condition/comparison_operator.py
@@ -0,0 +1,17 @@
+'''
+비교연산자: 두개의 값을 비교할 때 이용
+1. a==b : 같을 때 참을 반환
+2. a!=b: 같지 않을 때 참을 반환
+3. a>b, a<b, a>=b, a<=b: 두 개의 크기를 비교
+'''
+
+score = 82
+
+if score >= 90:
+  print("A학점")
+elif score >= 80:
+  print("B학점")
+elif socre >= 70:
+  print("C학점")
+else:
+  print("F학점")
