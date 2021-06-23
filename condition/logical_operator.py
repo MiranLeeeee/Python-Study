ce50accd9ea35e923c681ab1575e9329a12c308f
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-06-23 15:13:35 +0900

    Create logical_operator.py

---
+++ condition/logical_operator.py
@@ -0,0 +1,10 @@
+'''
+논리연산자: 논리 값 사이의 연산을 수행
+1. a and b: a와 b가 모두 True일 때 True반환
+2. a or b: a나 b중에 하나라도 True면 True반환
+3. not a: a가 False면 True, True면 False를 반환
+'''
+
+a = int(input("정수를 입력해주세요: "))
+if a<=50 and a>=0:
+  print("0이상 50이하의 정수를 입력하셨군요!.")
