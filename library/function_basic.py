3819aac321090ef4ba3221f0aba6a7b9a830d01e
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-07-14 15:46:01 +0900

    Create function_basic.py

---
+++ library/function_basic.py
@@ -0,0 +1,12 @@
+# 내장함수: 연산 등을 쉽게 구현
+
+total = sum([1,2,3,4,5,6,7,8,9,10])
+print("total: ", total)
+
+min_, max_ = min(1,4,2,3), max(1,4,2,3)
+print("최소: ", min_)
+print("최대: ", max_)
+
+# 따로 계산도 가능
+result = eval("(1+5)/2*6")
+print("결과: ", result)
